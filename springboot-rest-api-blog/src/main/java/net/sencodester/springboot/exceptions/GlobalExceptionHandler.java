7826de51f98bd41779a3bbe4f91f7a8c95d55250
Abraham <diallo.abraham@outlook.com>
2023-12-29 23:31:57 +0100

    Coding:amelioration de la methode GlobalExceptionHandler for delete Post BY id REST API and Testing Postman.

--- springboot-rest-api-blog/src/main/java/net/sencodester/springboot/exceptions/GlobalExceptionHandler.java
+++ springboot-rest-api-blog/src/main/java/net/sencodester/springboot/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package net.sencodester.springboot.exceptions;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
@@ -14,18 +16,38 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    // Gestionnaire pour ResourceNotFoundException
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", "Not Found");
-        body.put("message", ex.getMessage());
-        body.put("path", request.getDescription(false).replace("uri=", ""));
-
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        logger.error("ResourceNotFoundException: {}", ex.getMessage());
+        return buildResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
     }
 
-    // Ajoutez d'autres gestionnaires d'exceptions ici si nécessaire
+    // Gestionnaire pour les exceptions internes du serveur
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        logger.error("Exception: {}", ex.getMessage(), ex);
+        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred", request.getDescription(false));
+    }
 
+    // Gestionnaire pour les incompatibilités de types d'arguments
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        String message = String.format("The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+        logger.error("MethodArgumentTypeMismatchException: {}", message);
+        return buildResponseEntity(HttpStatus.BAD_REQUEST, message, request.getDescription(false));
+    }
+
+    // Méthode pour construire l'entité de réponse
+    private ResponseEntity<Object> buildResponseEntity(HttpStatus status, String message, String path) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+        body.put("path", path.replace("uri=", ""));
+        return new ResponseEntity<>(body, status);
+    }
 }
