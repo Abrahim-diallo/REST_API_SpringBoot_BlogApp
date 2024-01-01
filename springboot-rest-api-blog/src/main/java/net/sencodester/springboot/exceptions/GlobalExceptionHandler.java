958b6a7f9af9cacdd24a85a04fbbe68f91e937c2
Abraham <diallo.abraham@outlook.com>
2024-01-01 17:44:52 +0100

    Coding: Specific or Custom Exception Handling and Global Exception Handling et testing Postman.

--- springboot-rest-api-blog/src/main/java/net/sencodester/springboot/exceptions/GlobalExceptionHandler.java
+++ springboot-rest-api-blog/src/main/java/net/sencodester/springboot/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package net.sencodester.springboot.exceptions;
 
+import net.sencodester.springboot.payload.ErrorDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,47 +8,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    // Gestionnaire pour ResourceNotFoundException
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         logger.error("ResourceNotFoundException: {}", ex.getMessage());
-        return buildResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    // Gestionnaire pour les exceptions internes du serveur
+    // Autres handleBlogapiException...
+    @ExceptionHandler(BlogapiException.class)
+    public ResponseEntity<ErrorDetails> handleBlogapiException(BlogapiException ex, WebRequest request) {
+        logger.error("handleBlogapiException: {}", ex.getMessage());
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+    //handleGlobalException
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
-        logger.error("Exception: {}", ex.getMessage(), ex);
-        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred", request.getDescription(false));
-    }
-
-    // Gestionnaire pour les incompatibilités de types d'arguments
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
-        String message = String.format("The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
-        logger.error("MethodArgumentTypeMismatchException: {}", message);
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, message, request.getDescription(false));
-    }
-
-    // Méthode pour construire l'entité de réponse
-    private ResponseEntity<Object> buildResponseEntity(HttpStatus status, String message, String path) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", status.value());
-        body.put("error", status.getReasonPhrase());
-        body.put("message", message);
-        body.put("path", path.replace("uri=", ""));
-        return new ResponseEntity<>(body, status);
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, WebRequest request) {
+        logger.error("handleGlobalException: {}", ex.getMessage());
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
