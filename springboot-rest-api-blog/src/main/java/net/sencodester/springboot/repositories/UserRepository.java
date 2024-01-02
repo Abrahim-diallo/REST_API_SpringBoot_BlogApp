5d1a9d0417893b0966936f551750f17603540ef0
Abraham <diallo.abraham@outlook.com>
2024-01-02 14:06:16 +0100

    Coding: Creating JPA Repositories - UserRepository and RoleRepository et testing Postman.

---
+++ springboot-rest-api-blog/src/main/java/net/sencodester/springboot/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package net.sencodester.springboot.repositories;
+
+import net.sencodester.springboot.entites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+
+
+}
