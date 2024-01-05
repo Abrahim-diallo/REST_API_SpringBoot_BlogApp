692a0f772b7cadd34efa1a44b5c2afdf9318b10c
Abraham <diallo.abraham@outlook.com>
2024-01-05 14:44:43 +0100

    Coding: Reactoring et testing Postman.

--- springboot-rest-api-blog/src/main/java/net/sencodester/springboot/SpringbootRestApiBlogApplication.java
+++ springboot-rest-api-blog/src/main/java/net/sencodester/springboot/SpringbootRestApiBlogApplication.java
@@ -1,19 +1,38 @@
 package net.sencodester.springboot;
 
+import net.sencodester.springboot.entites.Role;
+import net.sencodester.springboot.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootRestApiBlogApplication {
-	@Bean
-	public ModelMapper modelMapper() {
-			return new ModelMapper();
-	}
+public class SpringbootRestApiBlogApplication implements CommandLineRunner {
+
+	@Autowired
+	private RoleRepository roleRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApiBlogApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Your initialization logic here
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
- 
